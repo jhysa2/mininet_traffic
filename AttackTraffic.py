778c935f6e9dbbddf3cc3f762f7c1b406e286f92
Joni Hysa <38633769+jhysa2@users.noreply.github.com>
2025-09-09 04:54:00 +0200

    Update AttackTraffic.py

--- AttackTraffic.py
+++ AttackTraffic.py
@@ -17,7 +17,7 @@ class DDoSConfig:
     CONTROLLER_IP = "10.0.2.5"
     
     # Default target is the server h1, but can be overridden
-    DEFAULT_TARGET_IP = "10.0.2.5"
+    DEFAULT_TARGET_IP = "10.0.0.1"
 
 def run_tcp_syn_flood(attacker, target_ip, duration_sec):
     """Commands the attacker to run a TCP SYN Flood."""
